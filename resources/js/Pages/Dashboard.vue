f2d235764cecd3bef05aa4e7c53525c3ff06fab8
Jaafar-digital <149582275+Jaafar-digital@users.noreply.github.com>
2024-01-28 14:49:31 +0100

    Adding project files inertia & vuetify

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,35 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+        </template>
+        <v-stepper :items="['Step 1', 'Step 2', 'Step 3']">
+            <template v-slot:item.1>
+                <v-card title="Step One" flat>...</v-card>
+            </template>
+
+            <template v-slot:item.2>
+                <v-card title="Step Two" flat>...</v-card>
+            </template>
+
+            <template v-slot:item.3>
+                <v-card title="Step Three" flat>...</v-card>
+            </template>
+        </v-stepper>
+        <v-date-picker></v-date-picker>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">You're logged in!</div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
